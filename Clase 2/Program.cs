6f1d560c5523d08d5fa1b8cee3345cb9c6eb54d8
Leandro <Leandro@DESKTOP-NEBTPF4>
2024-04-09 22:34:16 -0300

    Agregar archivos de proyecto.

---
+++ Clase 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace Clase_2
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                bool estado = true;
+
+                while (estado)
+                { 
+                Console.Write("Ingrese un número entero: ");
+                if (int.TryParse(Console.ReadLine(), out int numero))
+                {
+                    string tablaMultiplicar = ObtenerTablaMultiplicar(numero);
+                    Console.WriteLine(tablaMultiplicar);
+                    estado = false;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida. Debe ingresar un número entero.");
+                } 
+                }
+            }
+
+            static string ObtenerTablaMultiplicar(int numero)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Tabla de multiplicar del número {numero}:");
+                for (int i = 1; i <= 10; i++)
+                {
+                    sb.AppendLine($"{numero} x {i} = {numero * i}");
+                }
+                return sb.ToString();
+            }
+        
+    }
+}
---
+++ Clase1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Clase1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero;
+            string datoIngresado;
+            bool estado;
+            double numeroCuadrado;
+            double numeroCubo;
+            string mensaje = "Ingrese un numero: ";
+            string mensajeError = "Error ingrese un numero";
+
+            Console.Write(mensaje);
+            datoIngresado = Console.ReadLine();
+            estado = float.TryParse(datoIngresado, out numero);
+
+            while (!estado)
+            {
+             Console.WriteLine(mensajeError);
+             Console.Write(mensaje);
+             datoIngresado = Console.ReadLine();
+             estado = float.TryParse(datoIngresado, out numero);
+
+            }
+
+            if (estado && numero > 0)
+            {
+                numeroCuadrado = Math.Pow(numero, 2);
+                numeroCubo = Math.Pow(numero, 3);
+                Console.WriteLine(numeroCuadrado);
+                Console.WriteLine(numeroCubo);
+            }
+            
+          
+        }
+    }
+}
